2d7435a49b9a256f2b66934cfdf9918eb0e5b4f6
Nicholas Pappagalo <npappagalo@gmail.com>
2017-11-17 18:06:22 -0800

    style posts

--- imports/ui/components/Post/Post.js
+++ imports/ui/components/Post/Post.js
@@ -1,5 +1,7 @@
+/* eslint-disable camelcase */
+
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import enums from '../../../modules/enums';
 
@@ -10,20 +12,31 @@ class Post extends Component {
     this.props.onClick(this.props.item);
   };
   render() {
-    const { title, permalink, thumbnail } = this.props.item;
+    // reddit stores comments in snake case, my schema is camel case
+    const { title, permalink, thumbnail, numComments, num_comments } = this.props.item;
     const { isFavorite } = this.props;
     const redditLink = enums.redditUrl + permalink;
-
+    const src = thumbnail !== 'self' && thumbnail !== 'default' ? thumbnail : '/redditHolder.png';
     return (
-      <div>
-        <Button onClick={this.handleClick} className={isFavorite}>
-          Add
-        </Button>
-        <a href={redditLink} target="_blank">
-          {title}
-        </a>
-        <img src={thumbnail} alt={title} />
-      </div>
+      <Row className="post">
+        <Col sm={2} xsHidden>
+          <div className="post-img-container">
+            <img src={src} alt={title} className="post-img" />
+          </div>
+        </Col>
+        <Col className="post-text-container" sm={8} xs={10} smPush={0} xsPush={2}>
+          <a href={redditLink} target="_blank" className="post-text">
+            {title}
+          </a>
+          <p className="post-comments">{num_comments || numComments} comments</p>
+        </Col>
+        <Col xs={2} smPull={0} xsPull={10} className="text-center">
+          <i className="fa fa-ellipsis-v divider" />
+          <button onClick={this.handleClick} className={`${isFavorite} post-fav`}>
+            <span className="fa fa-star" />
+          </button>
+        </Col>
+      </Row>
     );
   }
 }
@@ -34,6 +47,8 @@ Post.propTypes = {
   item: PropTypes.shape({
     title: PropTypes.string.isRequired,
     permalink: PropTypes.string.isRequired,
+    numComments: PropTypes.number,
+    num_comments: PropTypes.number,
     thumbnail: PropTypes.string.isRequired
   }).isRequired
 };
--- imports/ui/layouts/App/App.js
+++ imports/ui/layouts/App/App.js
@@ -19,30 +19,32 @@ import Signup from '../../pages/Signup/Signup';
 import Login from '../../pages/Login/Login';
 
 import NotFound from '../../pages/NotFound/NotFound';
-import Footer from '../../components/Footer/Footer';
+// import Footer from '../../components/Footer/Footer';
 
 import './App.scss';
 
 const App = props => (
   <Router>
-    {!props.loading ? (
-      <div className="App">
-        <Navigation {...props} />
-        <Grid>
-          <Switch>
-            <Route exact name="index" path="/" component={Index} />
-            <Authenticated exact path="/hot" component={Hot} {...props} />
-            <Authenticated exact path="/likes" component={Likes} {...props} />
-            <Public path="/signup" component={Signup} {...props} />
-            <Public path="/login" component={Login} {...props} />
-            <Route component={NotFound} />
-          </Switch>
-        </Grid>
-        <Footer />
-      </div>
-    ) : (
-      ''
-    )}
+    <div id="wrapper">
+      {!props.loading ? (
+        <div className="App">
+          <Navigation {...props} />
+          <Grid>
+            <Switch>
+              <Route exact name="index" path="/" component={Index} />
+              <Authenticated exact path="/hot" component={Hot} {...props} />
+              <Authenticated exact path="/likes" component={Likes} {...props} />
+              <Public path="/signup" component={Signup} {...props} />
+              <Public path="/login" component={Login} {...props} />
+              <Route component={NotFound} />
+            </Switch>
+          </Grid>
+          {/* <Footer /> */}
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
   </Router>
 );
 
